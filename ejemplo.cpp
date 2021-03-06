4b751fdfbcc34f861da259f17914b68f4e73ddc4
ycamargob <ycamargob@unal.edu.co>
2021-03-06 15:48:31 -0500

    Primer cambio en repo actual Aparte de decir hola mundo, al insertar una variable almacena un valor

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include<cstdlib>
 
-int main(int arg, char **argv)
+// ./a.out -> argc=1, argv =["./a.out"]
+// ./a.out 10 -> argc = 2, argv =["./a.out", "10"]
+
+int main(int argc, char **argv)
 {
-    std::cout <<"Hola mundo";
+    int a = std::atoi(argv[1]);
+    a = 2*a;
+
+    std::cout << "Hola mundo";
+    std::cout << "2*a: "<< a << std::endl;
     return 0;
 }
