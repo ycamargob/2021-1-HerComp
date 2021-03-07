8e8b8218e27df22eb4b4f066414a0cc86f51c779
ycamargob <ycamargob@unal.edu.co>
2021-03-06 22:48:58 -0500

    Ejemplo de argumentos en la línea de comandos

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -4,12 +4,19 @@
 // ./a.out -> argc=1, argv =["./a.out"]
 // ./a.out 10 -> argc = 2, argv =["./a.out", "10"]
 
+void print_numbers(int min, int max);
+
 int main(int argc, char **argv)
 {
     int a = std::atoi(argv[1]);
-    a = 2*a;
-
-    std::cout << "Hola mundo";
-    std::cout << "2*a: "<< a << std::endl;
+    int b = std::atoi(argv[2]);
+    print_numbers(a,b);
     return 0;
+}
+
+void print_numbers(int min, int max)
+{
+    for (int ii=min; ii <= max; ++ii){
+        std::cout << ii << std::endl;
+    }
 }
